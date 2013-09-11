ae527c655b0ae74ffa09ad214a7a3d0f356ac84e
unknown <diego@brasa.art.br>
2013-09-11 13:09:28 -0300

    custom login function

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
 <?php
 require_once('library/siteframework.php');		// core functions
 require('theme-options.php');          			// theme options
+
+// Custom login
+function my_custom_login_logo() {
+    echo '<style type="text/css">
+        #login h1 a { height: 150px !important; margin-top: -3em !important;
+		}
+    </style>';
+}
+
+add_action('login_head', 'my_custom_login_logo');
+
+
 ?>
